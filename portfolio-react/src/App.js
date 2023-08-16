ca31ddaab9ee572496f2fb3a774f0cee76cb3969
Dean Heeger <127305010+DeanHeeger@users.noreply.github.com>
2023-08-16 10:19:15 +0200

    Update App.js

--- portfolio-react/src/App.js
+++ portfolio-react/src/App.js
@@ -1,21 +1,14 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import './App.css';
 import Navbar from './components/navbar';
 import Carousel from './components/Carousel';
-import Typed from 'typed.js';
+import { useTypewriter, Cursor } from 'react-simple-typewriter';
+//import Typed from 'typed.js';
 
 function App() {
-    const typedRef = useRef(null);
-
-    useEffect(() => {
-        new Typed(typedRef.current, {
-        strings:["Full Stack Developer", "Web Developer", "SQL Developer"],
-        typedSpeed:250,
-        backSpeed:250,
-        startDelay:100,
-        backDelay:100,
-        loop:true
-        });
+    const [text] = useTypewriter({
+        words: ['Full Stack Developer', 'Web Developer', 'SQL Developer'],
+        loop:{}
     });
 
   return (
@@ -37,7 +30,11 @@ function App() {
                             <div className="hero-text">
                                 <h5>Hi! I am Dean Heeger, a </h5>
                                 <div>
-                                   <span className="input" ref={typedRef}/> 
+                                   {/*<span className="input" ref={typedRef}/>*/}
+                                   <span className='input'>
+                                    {text}
+                                   </span>
+                                   <Cursor/>
                                 </div>
                                 <p>
                                     Ever since I was a little boy, I loved solving problems and helping others. 
@@ -115,11 +112,11 @@ function App() {
                             <div className="btn-group">
                                 <a href="./images/Dean Heeger CV.pdf" className="btn active">Download CV</a>
                             </div>
-                            <div class="social">
-                                <a href="https://www.instagram.com/dean_heeger/"><i class="fa-brands fa-instagram"></i></a>
-                                <a href="https://za.linkedin.com/in/dean-heeger-32a915170"><i class="fa-brands fa-linkedin"></i></a>
-                                <a href="https://wa.me/+27826912641"><i class="fa-brands fa-whatsapp"></i></a>
-                                <a href="https://github.com/DeanHeeger?tab=repositories"><i class="fa-brands fa-github"></i></a>
+                            <div className="social">
+                                <a href="https://www.instagram.com/dean_heeger/"><i className="fa-brands fa-instagram"></i></a>
+                                <a href="https://za.linkedin.com/in/dean-heeger-32a915170"><i className="fa-brands fa-linkedin"></i></a>
+                                <a href="https://wa.me/+27826912641"><i className="fa-brands fa-whatsapp"></i></a>
+                                <a href="https://github.com/DeanHeeger?tab=repositories"><i className="fa-brands fa-github"></i></a>
                             </div>
                         </div>
                     </div>
@@ -152,10 +149,10 @@ function App() {
                             <a href="/" className="btn">CLEAR</a>
                         </div>
                         <div className="social">
-                            <a href="https://www.instagram.com/dean_heeger/"><i class="fa-brands fa-instagram"></i></a>
-                            <a href="https://za.linkedin.com/in/dean-heeger-32a915170"><i class="fa-brands fa-linkedin"></i></a>
-                            <a href="https://wa.me/+27826912641"><i class="fa-brands fa-whatsapp"></i></a>
-                            <a href="https://github.com/DeanHeeger?tab=repositories"><i class="fa-brands fa-github"></i></a>
+                            <a href="https://www.instagram.com/dean_heeger/"><i className="fa-brands fa-instagram"></i></a>
+                            <a href="https://za.linkedin.com/in/dean-heeger-32a915170"><i className="fa-brands fa-linkedin"></i></a>
+                            <a href="https://wa.me/+27826912641"><i className="fa-brands fa-whatsapp"></i></a>
+                            <a href="https://github.com/DeanHeeger?tab=repositories"><i className="fa-brands fa-github"></i></a>
                         </div>
                     </div>
                 </div>
