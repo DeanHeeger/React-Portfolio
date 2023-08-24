b3cd83f91cfdfb6c357942c41ddae7e716386e42
Dean Heeger <127305010+DeanHeeger@users.noreply.github.com>
2023-08-24 14:23:53 +0200

    About component
    Created a component for the About section.

--- portfolio-react/src/App.js
+++ portfolio-react/src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/navbar';
 import Carousel from './components/Carousel';
 import Contact from './components/Contact';
 import Social from './components/Social';
+import About from './components/About';
 import Typed from 'typed.js';
 
 function App() {
@@ -83,49 +84,7 @@ function App() {
                 <section id="about">
                     <div className="container">
                         <div className="hero-text">
-                            <h1>About Me</h1>
-
-                            <p>
-                                My name is Dean Heeger, I am 23 and I am a developer.
-                                From a young age, the desire to solve problems and help others has been an integral 
-                                part of who I am. As a little boy, I was always fascinated with the inner workings of 
-                                things, and I would often take them apart just to see how they functioned. This 
-                                curiosity fueled my passion for technology, and I became enamored with the rapid 
-                                growth of the industry.
-                            </p>
-                            <br/>
-                            <p>
-                                Over time, my interest in technology grew stronger, and I began to explore various 
-                                programming languages such as HTML, CSS, JavaScript, Python, C#, and SQL. These languages 
-                                allowed me to tackle complex software challenges and deliver innovative solutions that 
-                                exceeded client expectations.
-                            </p>
-                            <br/>
-                            <p>
-                                My background in programming languages has provided me with a unique set of skills that allows 
-                                me to approach problems from multiple perspectives. I am able to analyze complex systems and 
-                                break them down into smaller, manageable components. This ability to think critically has 
-                                been invaluable in my work as a problem solver.
-                            </p>
-                            <br/>
-                            <p>
-                                As a software developer, my love for technology has translated into a passion for creating 
-                                solutions that make life easier for others. Whether it's a mobile application that simplifies 
-                                daily tasks or a website that provides easy access to important information, I am constantly 
-                                striving to make a positive impact on the world through my work.
-                            </p>
-                            <br/>
-                            <p>
-                                In conclusion, my passion for solving problems and helping others has been a driving force in my life. 
-                                My love of technology has allowed me to pursue a career in software development, where I can use my 
-                                skills to make a difference in the world. With a strong foundation in programming languages and a 
-                                commitment to delivering innovative solutions, I am excited to continue growing and expanding my 
-                                skills as a problem solver and technology enthusiast.
-                            </p>
-                            
-                            <div className="btn-group">
-                                <a href="./images/Dean Heeger CV.pdf" className="btn active">Download CV</a>
-                            </div>
+                            <About/>
                             <Social/>
                         </div>
                     </div>
---
+++ portfolio-react/src/components/About.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+const About = () => {
+    return(
+        <div>
+            <h1>About Me</h1>
+            <p>
+                My name is Dean Heeger, I am 23 and I am a developer.
+                From a young age, the desire to solve problems and help others has been an integral 
+                part of who I am. As a little boy, I was always fascinated with the inner workings of 
+                things, and I would often take them apart just to see how they functioned. This 
+                curiosity fueled my passion for technology, and I became enamored with the rapid 
+                growth of the industry.
+            </p>
+            <br/>
+            <p>
+                Over time, my interest in technology grew stronger, and I began to explore various 
+                programming languages such as HTML, CSS, JavaScript, Python, C#, and SQL. These languages 
+                allowed me to tackle complex software challenges and deliver innovative solutions that 
+                exceeded client expectations.
+            </p>
+            <br/>
+            <p>
+                My background in programming languages has provided me with a unique set of skills that allows 
+                me to approach problems from multiple perspectives. I am able to analyze complex systems and 
+                break them down into smaller, manageable components. This ability to think critically has 
+                been invaluable in my work as a problem solver.
+            </p>
+            <br/>
+            <p>
+                As a software developer, my love for technology has translated into a passion for creating 
+                solutions that make life easier for others. Whether it's a mobile application that simplifies 
+                daily tasks or a website that provides easy access to important information, I am constantly 
+                striving to make a positive impact on the world through my work.
+            </p>
+            <br/>
+            <p>
+                In conclusion, my passion for solving problems and helping others has been a driving force in my life. 
+                My love of technology has allowed me to pursue a career in software development, where I can use my 
+                skills to make a difference in the world. With a strong foundation in programming languages and a 
+                commitment to delivering innovative solutions, I am excited to continue growing and expanding my 
+                skills as a problem solver and technology enthusiast.
+            </p>
+
+            <div className="btn-group">
+                <a href="./images/Dean Heeger CV.pdf" className="btn active">Download CV</a>
+            </div>
+        </div>
+    )
+}
+
+export default About;
