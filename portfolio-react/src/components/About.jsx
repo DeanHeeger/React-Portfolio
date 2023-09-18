8bdebd14b244e5b382dd715378caaaeb9f696442
Dean Heeger <127305010+DeanHeeger@users.noreply.github.com>
2023-09-18 11:06:35 +0200

    Update About.jsx
    Highlight important text in About.

--- portfolio-react/src/components/About.jsx
+++ portfolio-react/src/components/About.jsx
@@ -5,41 +5,41 @@ const About = () => {
         <div>
             <h1>About Me</h1>
             <p>
-                My name is Dean Heeger, I am 23 and I am a developer.
+                My name is <span style={{color: '#ffc300'}}>Dean Heeger</span>, I am 23 and I am a <span style={{color: '#ffc300'}}>software developer</span>.
                 From a young age, the desire to solve problems and help others has been an integral 
                 part of who I am. As a little boy, I was always fascinated with the inner workings of 
                 things, and I would often take them apart just to see how they functioned. This 
-                curiosity fueled my passion for technology, and I became enamored with the rapid 
+                curiosity fueled my passion for technology, and I became <span style={{color: '#ffc300'}}>enamored </span> with the rapid 
                 growth of the industry.
             </p>
             <br/>
             <p>
                 Over time, my interest in technology grew stronger, and I began to explore various 
-                programming languages such as HTML, CSS, JavaScript, Python, C#, and SQL. These languages 
+                programming languages such as <span style={{color: '#ffc300'}}>HTML, CSS, JavaScript, Python, React Native, and SQL</span>. These languages 
                 allowed me to tackle complex software challenges and deliver innovative solutions that 
                 exceeded client expectations.
             </p>
             <br/>
             <p>
                 My background in programming languages has provided me with a unique set of skills that allows 
-                me to approach problems from multiple perspectives. I am able to analyze complex systems and 
-                break them down into smaller, manageable components. This ability to think critically has 
-                been invaluable in my work as a problem solver.
+                me to approach problems from multiple perspectives. I am able to <span style={{color: '#ffc300'}}>analyze</span> complex systems and 
+                break them down into smaller, manageable components. This ability to <span style={{color: '#ffc300'}}>think critically</span> has 
+                been invaluable in my work as a <span style={{color: '#ffc300'}}>problem solver</span>.
             </p>
             <br/>
             <p>
-                As a software developer, my love for technology has translated into a passion for creating 
+                As a software developer, my <span style={{color: '#ffc300'}}>love for technology</span> has translated into a <span style={{color: '#ffc300'}}>passion for creating </span> 
                 solutions that make life easier for others. Whether it's a mobile application that simplifies 
-                daily tasks or a website that provides easy access to important information, I am constantly 
-                striving to make a positive impact on the world through my work.
+                daily tasks or a website that provides easy access to important information, I am <span style={{color: '#ffc300'}}>constantly 
+                striving </span> to make a positive impact on the world through my work.
             </p>
             <br/>
             <p>
                 In conclusion, my passion for solving problems and helping others has been a driving force in my life. 
                 My love of technology has allowed me to pursue a career in software development, where I can use my 
-                skills to make a difference in the world. With a strong foundation in programming languages and a 
+                skills to make a <span style={{color: '#ffc300'}}>difference in the world</span>. With a strong foundation in programming languages and a 
                 commitment to delivering innovative solutions, I am excited to continue growing and expanding my 
-                skills as a problem solver and technology enthusiast.
+                skills as a problem solver and <span style={{color: '#ffc300'}}>technology enthusiast</span>.
             </p>
 
             <div className="btn-group">
